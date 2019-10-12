016fa2acc4ab3a9e38dfa06682e9ea38c08a7398
Jan Goebel <jan.goebel.dev@gmail.com>
2019-10-12 17:45:00 +0200

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/logo.png';
+import styles from './LandingPage.module.css'
 
 export function LandingPage() {
     return (
         <div>
             <TopNav/>
+            <img src={logo} className={styles.logo} alt='logo'/>
         </div>
     );
 }
