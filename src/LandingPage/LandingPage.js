fb11f415cc725318018a1998964a0c805c42a04c
Jan Goebel <jan.goebel.dev@gmail.com>
2019-10-13 15:47:46 +0200

    adapt layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,11 +7,13 @@ import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 
 export function LandingPage() {
     return (
-        <div>
-            <TopNav/>
-            <img src={logo} className={styles.logo} alt='logo'/>
-            <SearchBar/>
-            <SearchSuggestions/>
+        <div className={styles.landing}>
+            <div className={styles['search-area']}>
+                <TopNav/>
+                <img src={logo} className={styles.logo} alt='logo'/>
+                <SearchBar/>
+                <SearchSuggestions/>
+            </div>
         </div>
     );
 }
