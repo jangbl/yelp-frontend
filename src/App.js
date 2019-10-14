f0198081c6747085631794baf92ecfcf7b474b3c
Jan Goebel <jan.goebel.dev@gmail.com>
2019-10-14 21:41:05 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { LandingPage } from './LandingPage/LandingPage';
+import {Route, Switch} from 'react-router-dom';
+import {Search} from './Search/Search';
 
 function App() {
   return (
-    <LandingPage/>
+    <Switch>
+      <Route path='/search' component={Search}/>
+      <Route path='/' component={LandingPage}/>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
