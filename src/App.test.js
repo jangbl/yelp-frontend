7763fce589a6a509f383b033f815b2c36ab6782d
Jan Goebel <jan@productioncoder.com>
2020-02-29 11:36:23 +0100

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
