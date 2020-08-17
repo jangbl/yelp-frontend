460bfeaafb213c8bc8ae2273d75514885d3038f5
productioncoder <jan@productioncoder.com>
2020-08-17 11:21:48 +0200

    cosmetics + update dependencies

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
 import logo from '../assets/logo.png';
-import styles from './LandingPage.module.css'
+import styles from './LandingPage.module.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 import useReactRouter from 'use-react-router';
 
 export function LandingPage() {
-    const {history} = useReactRouter();
+  const { history } = useReactRouter();
 
-    function search(term, location) {
-        const urlEncodedTerm = encodeURI(term);
-        const urlEncodedLocation = encodeURI(location);
-        history.push(`/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`);
-    }
-
-    return (
-        <div className={styles.landing}>
-            <div className={styles['search-area']}>
-                <TopNav/>
-                <img src={logo} className={styles.logo} alt='logo'/>
-                <SearchBar search={search} />
-                <SearchSuggestions/>
-            </div>
-        </div>
+  function search(term, location) {
+    const urlEncodedTerm = encodeURI(term);
+    const urlEncodedLocation = encodeURI(location);
+    history.push(
+      `/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`
     );
+  }
+
+  return (
+    <div className={styles.landing}>
+      <div className={styles['search-area']}>
+        <TopNav />
+        <img src={logo} className={styles.logo} alt="logo" />
+        <SearchBar search={search} />
+        <SearchSuggestions />
+      </div>
+    </div>
+  );
 }
